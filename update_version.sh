c877d11a43f2d8e3cb19f1259887b0d570b6fb2d
Sybren A. Stüvel <sybren@stuvel.eu>
2017-03-16 10:24:09 +0100

    Added standard git commands for committing & tagging version bump

--- update_version.sh
+++ update_version.sh
@@ -10,4 +10,6 @@ sed "s/__version__\s*=\s*'[^']*'/__version__ = '$1'/" -i flamenco_worker/__init_
 
 git diff
 echo
-echo "Don't forget to tag and commit!"
+echo "Don't forget to commit and tag:"
+echo git commit -m \'Bumped version to $1\' setup.py flamenco_worker/__init__.py
+echo git tag -a v$1 -m \'Tagged version $1\'
