9d83e42726cf0a81ee4b6749db7440d47d187026
Sybren A. Stüvel <sybren@stuvel.eu>
2017-06-09 14:35:45 +0200

    Added publish-online script

---
+++ publish-online.sh
@@ -0,0 +1,12 @@
+#!/bin/bash -e
+
+FLAMENCO_VERSION=2.0.5
+echo "Uploading Flamenco Worker $FLAMENCO_VERSION to flamenco.io"
+
+cd dist
+rsync \
+    flamenco-worker-${FLAMENCO_VERSION}.zip \
+    flamenco-worker-${FLAMENCO_VERSION}.sha256 \
+    armadillica@flamenco.io:flamenco.io/download/ -va
+
+echo "Done!"
